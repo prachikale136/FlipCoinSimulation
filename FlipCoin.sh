3b50cbefd8c28b98d16b40e4b191e8ff6a97ef4c
Prachi <prachikale136@gmail.com>
2023-02-05 16:24:55 +0530

    display head or tail

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,2 +1,11 @@
-echo "this display the winner Heads or Tails"
+echo "this display the winner Heads or Tail"
 
+#!/bin/bash -x
+
+flip=$(($RANDOM%2))
+if [ $flip -eq 1 ]
+then
+	echo "HEADS"
+else
+	echo "TAILS"
+fi
