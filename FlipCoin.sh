d5f5beeeb17f2cd0accc87c33e50f1a6d62c8491
Prachi <prachikale136@gmail.com>
2023-02-05 17:47:27 +0530

    head tail display using loop

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,11 +1,23 @@
 echo "this display the winner Heads or Tail"
 
 #!/bin/bash -x
+counter=0;
+totalHeads=0;
+totalTails=0;
+read -p "Enter how many times to flip the coin " flip
+while [ $counter -le $flip ]
+do
+	coin=$(($RANDOM%2))
+	if [ $coin -eq 1 ]
+	then
+		echo "HEADS IS WINNER"
+		((totalHeads++))
+	else
+		echo "TAILS IS WINNER"
+		((totalTails++))
+	fi
+	((counter++))
+done
 
-flip=$(($RANDOM%2))
-if [ $flip -eq 1 ]
-then
-	echo "HEADS is winner"
-else
-	echo "TAILS is winner"
-fi
+echo "Head win :" $totalHeads
+echo "Tail win: " $totalTails
